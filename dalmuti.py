70d428c467d114de95d686055330bfc6b9a895a2
XNEP1 <xnep1@hotmail.com>
2023-06-15 04:42:34 -0300

    fix e exemplo de client-server

--- dalmuti.py
+++ dalmuti.py
@@ -2,12 +2,18 @@
 
 import socket # importa o módulo de sockets
 
+# Será lido do arquivo dalmuti.ini
+hostnameFrom = 'h1'  # Hostname de quem você recebe. Será convertido para o endereço IP
+hostnameTo = 'h1'  # Hostname de quem você envia. Será convertido para o endereço IP
 
-def setup_connection ():
+def setup_connection (porta):
     #
     #
     #
-    return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip = socket.gethostbyname(socket.gethostname())
+    UDPsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPsocket.bind((ip, porta))
+    return UDPsocket
 
 
 def send_to (socket, msg, ip_addr, port):
@@ -17,10 +23,18 @@ def send_to (socket, msg, ip_addr, port):
 
 def recv_from (socket, buffer_size):
     # caso o tamanho do buffer não seja um valor (numérico) inteiro
-    if (not buffer_size.is_integer())
-        # atribua um valor padrão (2048 nesse caso)
-        buffer_size = 2048
     #
-    (data_str, ip_addr) = socket.recvfrom(bytes())
+    (data_str, ip_addr) = socket.recvfrom(buffer_size)
     return (data_str, ip_addr) # retorna tupla com mensagem, e IP de origrm
+
+# Exemplo: SERVIDOR RECEBENDO
+# UDPsocket = setup_connection(7304)
+# while(True):
+    # (data_str, ip_addr) = recv_from(UDPsocket, 2048)
+    # print(data_str.decode('utf-8'))
+
+
+# Exemplo: CLIENT ENVIANDO
+# UDPsocket = setup_connection(7304)
+# send_to(UDPsocket, "Ola", socket.gethostbyname(hostnameTo), 7304);   
     
