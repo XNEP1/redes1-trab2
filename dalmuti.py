a02a26f36ecb938d62380d7f4d61c24908464f37
XNEP1 <xnep1@hotmail.com>
2023-06-16 02:09:07 -0300

    wip

--- dalmuti.py
+++ dalmuti.py
@@ -6,20 +6,35 @@ import sys
 
 import readconfig
 from tokenring import *
+
+def mensagem(Evento, Info = {}):
+    message = {
+        "Evento" : Evento,
+        "Info" : Info.copy()
+    }
+    return message
+
+def imprimir_tela():
+    pass
         
-class Eventos(Enum):
+class Evento(Enum):
     TOKEN = 0
     DESLIGAMENTO = 1
     JOGADA = 2
     VITORIA = 3
+    PING = 4 # Não faz nada, é só pra ver se a mensagem volta para você
+    OK = 4
 
-class Estados(Enum):
-    ESPERANDO = 0  # Esperando todo mundo se conectar
-    TURNO_DE_OUTRO = 1
-    MEU_TURNO = 2
-    VITORIA = 3
-    DERROTA = 4
-    FIM_DE_JOGO = 5
+class Estado(Enum):
+    ARRUMANDO_BARALHO = -2  # Você é o lider da mesa e está esperando todo mundo se conectar
+    INICIANDO_MASTER = -1   # Você é o lider da mesa e está distribuindo cartas
+    ESPERANDO = 0           # Esperando todo mundo se conectar
+    INICIANDO_PLAYER = 1    # Recebendo as cartas, etc
+    TURNO_DE_OUTRO = 2
+    MEU_TURNO = 3
+    VITORIA = 4
+    DERROTA = 5
+    FIM_DE_JOGO = 6
 
 class Carta:
     def __init__(self, valor):
@@ -28,7 +43,7 @@ class Carta:
 class Jogo:
 
     def __init__(self):
-        self.estado = Estados.ESPERANDO
+        self.estado = Estado.ESPERANDO
         self.turno = ""
         self.minhaMao = []
         self.registro = []
@@ -40,21 +55,42 @@ def jogo_principal ():
     tokenRing = TokenRing(From="h1", To="h2")
     jogo = inicializa_jogo()
     data = {}
+
+
     
-    while (jogo.estado != jogo.FIM_JOGO):
+    while (jogo.estado != Estado.FIM_DE_JOGO):
         imprimir_tela()
-        estado = jogo.estado
-        if estado == Estados.ESPERANDO:
+
+        if jogo.estado == Estado.ARRUMANDO_BARALHO:
+            while( not tokenRing.enviar(mensagem(Evento.PING), To=socket.gethostbyname(socket.gethostname()))):
+                pass
+            _ = tokenRing.receber()
+            tokenRing.enviar(mensagem(Evento.OK), To= "Broadcast") 
+            jogo.estado = Estado.INICIANDO_MASTER
+            continue
+
+        elif jogo.estado == Estado.ESPERANDO:
+            while( tokenRing.receber()["Evento"] != Evento.OK):
+                pass
+            jogo.estado = Estado.INICIANDO_PLAYER
+            continue
+
+
+        elif jogo.estado == Estado.INICIANDO_MASTER:
             pass # ...
-        elif estado == Estados.TURNO_OUTRO:
+        elif jogo.estado == Estado.INICIANDO_PLAYER:
             pass # ...
-        elif estado == Estados.MEU_TURNO:
+
+
+        elif jogo.estado == Estado.TURNO_DE_OUTRO:
             pass # ...
-        elif estado == Estados.VITORIA:
+        elif jogo.estado == Estado.MEU_TURNO:
             pass # ...
-        elif estado == Estados.DERROTA:
+        elif jogo.estado == Estado.VITORIA:
             pass # ...
-        elif estado == Estados.FIM_DE_JOGO:
+        elif jogo.estado == Estado.DERROTA:
+            pass # ...
+        elif jogo.estado == Estado.FIM_DE_JOGO:
             pass # ...
         else:
             pass # ...
