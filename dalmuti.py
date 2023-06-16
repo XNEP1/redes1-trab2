9612aedcbd0ce2193417c65d38d6ee736c2681d7
XNEP1 <xnep1@hotmail.com>
2023-06-15 22:27:33 -0300

    class TokenRing montada

--- dalmuti.py
+++ dalmuti.py
@@ -5,83 +5,91 @@ from threading import *
 from enum import Enum
 from queue import *
 import json
+import sys
 
 import readconfig
 import tokenring
-
-# Será lido do arquivo dalmuti.ini
-hostnameFrom = 'h1'  # Hostname de quem você recebe. Será convertido para o endereço IP
-hostnameTo = 'h1'  # Hostname de quem você envia. Será convertido para o endereço IP
-PORT = 7304
-have_token = False
-sendLock = Lock()
-messageQueue = Queue()
-my_addr = socket.gethostbyname(socket.gethostname())
-
-def receiveManager(UDPsocket):
-        while(True):
-            (data_str, ip_addr) = recv_from(UDPsocket, PORT)
-            data = json.loads(data_str.decode("utf-8"))
-            if data["Event"] == "Token":
-                # Pega permissao de enviar mensagens
-                break
-
-            if data["Event"] == "Desligamento":
-                # Mensagem especial de desligamento da rede
-                # Passa pro proximo e da exit
-                # função_exit(0)
-                pass
-
-            if data["To"] == my_addr:
-                # Se a thread principal nao funcionar,
-                # essa thread trava aqui e a rede em anel trava
-                messageQueue.put(data_str, block=True)
-
-            if data["From"] == my_addr:
-                # Remove do anel
-                sendLock.release()
-            else:
-                send_to(UDPsocket, data_str, hostnameTo, PORT)
-
-def enviar(data):
-    if have_token == False:
-        return False
-    send_to(UDPsocket, json.dumps(data), next_adrr, PORT)
-    sendLock.acquire()
-    return True
-
-def receber():
-    return messageQueue.get(block=True)
-
-def setup_connection (porta):
-    #
-    #
-    #
-    UDPsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    UDPsocket.bind((my_addr, porta))
-    return UDPsocket
-
-
-def send_to (socket, msg, ip_addr, port):
-    # envia a mensagem pelo socket como uma string na codificação UTF-8
-    # para a porta e endereço IPv4 passados como parâmetro
-    socket.sendto(bytes(msg, "utf-8"), (ip_addr, port))
-
-def recv_from (socket, buffer_size):
-    # caso o tamanho do buffer não seja um valor (numérico) inteiro
-    #
-    (data_str, ip_addr) = socket.recvfrom(buffer_size)
-    return (data_str, ip_addr) # retorna tupla com mensagem, e IP de origrm
 
 class TokenRing:
     def __init__(self, hostnameFrom, hostnameTo, port=7304):
         self.hostnameFrom = hostnameFrom
         self.hostnameTo = hostnameTo
+        self.from_addr = socket.gethostbyname(self.hostnameFrom)
+        self.to_addr = socket.gethostbyname(self.hostnameTo)
         self.port = port
-        self.UDPsocket = __setup_connection()
+        self.sendLock = Lock()
+        self.messageQueue = Queue()
+        self.have_token = False
+        self.my_addr = socket.gethostbyname(socket.gethostname())
+        self.UDPsocket = self.__setup_connection()
+        self.__startReceiver()
 
-    @staticmethod
-    def __setup_connection()
+    def enviar(self, data: dict) -> bool:
+        if self.have_token == False:
+            return False
+        socket.sendto(json.dumps(data).encode("utf-8"), (self.to_addr, self.port))
+        self.sendLock.acquire()
+        return True
+    
+    def receber(self) -> dict:
+        return self.messageQueue.get(block=True)
+    
+    # Essa função só deve ser usada pelo primeiro computador na lista do .ini
+    def criarToken(self):
+        self.have_token = True
+        return
+    
+    def passarToken(self):
+        data = {
+            "Event" : "Token"
+        }
+        socket.sendto(json.dumps(data).encode("utf-8"), (self.to_addr, self.port))
+        self.have_token = False
+        return
+    
+    def desligarRede(self):
+        data = {
+            "Event" : "Desligamento"
+        }
+        socket.sendto(json.dumps(data).encode("utf-8"), (self.to_addr, self.port))
+        return
+
+    def __setup_connection(self) -> socket:
+        UDPsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        UDPsocket.bind((self.my_addr, self.port))
+        return UDPsocket
+    
+    def __startReceiver(self):
+        self.recvManagerThread = Thread(target=self.__recvManager, args=[])
+        self.recvManagerThread.start()
+        return
+
+    def __recvManager(self):
+        while(True):
+            (data_str, recv_addr) = socket.recvfrom(2048)
+            if recv_addr != self.from_addr:
+                # Ignora
+                continue
+
+            data = json.loads(data_str.decode("utf-8"))
+            if data["Event"] == "Token":
+                self.have_token = True
+                continue
+
+            if data["Event"] == "Desligamento":
+                socket.sendto(data_str, (self.to_addr, self.port))
+                sys.exit()
+
+            if data["To"] == self.my_addr:
+                # Se a thread principal nao funcionar,
+                # essa thread trava aqui e a rede em anel trava
+                self.messageQueue.put(data_str, block=True)
+
+            if data["From"] == self.my_addr:
+                # Remove do anel
+                self.sendLock.release()
+            else:
+                socket.sendto(data_str, (self.to_addr, self.port))
         
 
 class Eventos(Enum):
